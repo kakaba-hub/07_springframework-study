f0b322bdef38353bd03ee43c132f27d584efeb49
Kyungbae_Go <gokyungbae@gmail.com>
2025-05-07 17:49:29 +0900

    create: Spring Framework 수업

---
+++ kyungbae-springframework-workspace/01_ioc-container/src/main/java/com/kyungbae/ioc/section02/javaconfig/Music.java
@@ -0,0 +1,13 @@
+package com.kyungbae.ioc.section02.javaconfig;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Music {
+    private String title;
+    private String genre;
+}
