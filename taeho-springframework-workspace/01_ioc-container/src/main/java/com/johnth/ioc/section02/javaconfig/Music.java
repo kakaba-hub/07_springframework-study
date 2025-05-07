c8f90bd9af608620d6122c7096fbc947f9f3bf32
THD <ok821000@gmail.com>
2025-05-08 08:29:54 +0900

    [Add] Spring 학습진행 - IOC 및 DI에 대한 이해

---
+++ taeho-springframework-workspace/01_ioc-container/src/main/java/com/johnth/ioc/section02/javaconfig/Music.java
@@ -0,0 +1,15 @@
+package com.johnth.ioc.section02.javaconfig;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Music {
+    private String title;
+    private String genre;
+}
