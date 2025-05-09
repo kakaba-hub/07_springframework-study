30dfb237128033a55bc93d72c92a77856a2ba450
Kyungbae_Go <gokyungbae@gmail.com>
2025-05-09 16:28:51 +0900

    create: webmvc end

--- kyungbae-springframework-workspace/03_webmvc/src/main/java/com/kyungbae/webmvc/controller/MvcController.java
+++ kyungbae-springframework-workspace/03_webmvc/src/main/java/com/kyungbae/webmvc/controller/MvcController.java
@@ -3,6 +3,8 @@ package com.kyungbae.webmvc.controller;
 
 // "/" url 요청시 /WEB-INF/views/main.jsp 가 보여지도록 forwarding 하기
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,9 +15,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MvcController {
 
+    private Logger logger = LoggerFactory.getLogger(MvcController.class);
+
     @RequestMapping(value = {"/", "/main", "/main.do"}, method = RequestMethod.GET)
     public String welcomePage(){
-        System.out.println("mc-welcomePage");
+        // System.out.println("mc-welcomePage");
+
+//        logger.trace("trace message - 가장 상세한 정보");
+//        logger.debug("debug message - 개발단계 디버깅 용도");
+//        logger.info("info message - 정보성 메세지");
+//        logger.warn("warn message - 당장엔 문제가 없지만 시스템 에러의 원인이 될 수 있는 경고");
+//        logger.error("error message - 요청 처리 중 문제가 발생한 상태를 기록할 용도");
+
         return "main";
     }
 
---
+++ kyungbae-springframework-workspace/03_webmvc/src/main/java/com/kyungbae/webmvc/dto/MemberDto.java
@@ -0,0 +1,16 @@
+package com.kyungbae.webmvc.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class MemberDto {
+
+    private String name;
+    private int age;
+    private String address;
+
+}
