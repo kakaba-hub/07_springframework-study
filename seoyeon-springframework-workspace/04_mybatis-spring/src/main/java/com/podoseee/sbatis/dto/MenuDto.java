fca5d0360e79765b34980f1ac09d40f723b1975c
Seoyeon <106144694+podoseee@users.noreply.github.com>
2025-05-13 09:01:53 +0900

    create 04_mybatis-spring

---
+++ seoyeon-springframework-workspace/04_mybatis-spring/src/main/java/com/podoseee/sbatis/dto/MenuDto.java
@@ -0,0 +1,17 @@
+package com.podoseee.sbatis.dto;
+
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class MenuDto {
+    private int menuCode;
+    private String menuName;
+    private int menuPrice;
+    private int categoryCode;
+    private String orderableStatus;
+}
