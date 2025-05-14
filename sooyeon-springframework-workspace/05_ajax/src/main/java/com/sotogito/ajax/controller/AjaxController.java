d782bc96448b28e042837529c4f01cb5aaf30362
sotogito <sotogitoarchive@gmail.com>
2025-05-14 11:04:18 +0900

    feat : ajax DTO

--- sooyeon-springframework-workspace/05_ajax/src/main/java/com/sotogito/ajax/controller/AjaxController.java
+++ sooyeon-springframework-workspace/05_ajax/src/main/java/com/sotogito/ajax/controller/AjaxController.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Slf4j
 @Controller
 public class AjaxController {
@@ -18,8 +20,8 @@ public class AjaxController {
     @GetMapping(value = "/ajaxTest1.do", produces = "text/html; charset=utf-8")
     public String ajaxTest1(@RequestParam String id, @RequestParam String pwd) {
         log.debug("param id:{}, pwd:{}", id, pwd);
-
         String searchedName = "홍길동";
+
         return searchedName;
     }
 
@@ -27,8 +29,8 @@ public class AjaxController {
     @PostMapping(value = "/ajaxTest2.do", produces = "text/html; charset=utf-8")
     public String ajaxTest2(UserDto user) {
         log.debug("param id:{}, pwd:{}", user.getId(), user.getPwd());
-
         String searchedName = "홍길동";
+
         return searchedName;
     }
 
@@ -36,11 +38,32 @@ public class AjaxController {
     @PostMapping(value = "/ajaxTest3.do", produces = "text/html; charset=utf-8")
     public String ajaxTest3(@RequestBody UserDto user) {
         log.debug("param id:{}, pwd:{}", user.getId(), user.getPwd());
-
         String searchedName = "3";
+
         return searchedName;
     }
 
+    @ResponseBody
+    @GetMapping(value = "/ajaxTest4.do")
+    public UserDto ajaxTest4(int no) {
+        log.debug("param no:{}", no);
+        UserDto searchedUser = new UserDto("test", "pass", "홍길순", 10);
+
+        return searchedUser;
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/ajaxTest5.do")
+    public List<UserDto> ajaxTest5() {
+        List<UserDto> selectedUsers = List.of(                              //[
+                new UserDto("user1","pass2","1",1),  //{}
+                new UserDto("user2","pass3","2",2),
+                new UserDto("user3","pass4","3",3),
+                new UserDto("user4","pass5","4",4),
+                new UserDto("user5","pass6","5",5)
+        );
+        return selectedUsers;
+    }
 
 
 }
